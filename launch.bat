a0a8d00e5116c83b238aa9c32e11aa8685563b7b
Aditya Tripathi <adityatripathi.at04@gmail.com>
2024-06-30 13:09:38 +0530

    Refactor launch.bat file

--- launch.bat
+++
@@ -1,7 +0,0 @@
-ampy -b 115200 -p COM3 put _global.py
-ampy -b 115200 -p COM3 put boot.py
-ampy -b 115200 -p COM3 put hcsr04.py
-ampy -b 115200 -p COM3 put main.py
-ampy -b 115200 -p COM3 put mfrc522.py
-ampy -b 115200 -p COM3 put read.py
-ampy -b 115200 -p COM3 put write.py
